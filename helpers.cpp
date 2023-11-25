e3ca1bbea853f567a970cc724cc0f2cb52aaf013
Youssef OUMATE <ysf.ing.ysf@gmail.com>
2023-11-25 22:37:42 +0900

    fix syntax error

--- helpers.cpp
+++ helpers.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+using namespace std;
 
 bool check_valid_assignment(int* assignment_index, int N){
   bool done = true;
@@ -9,27 +10,41 @@ bool check_valid_assignment(int* assignment_index, int N){
   }
   return done;
 }
-void print_matrix(int (&Cost)[4][4], int N, int M){
+bool inlist(tuple<int, int> coord, vector<tuple<int, int> > starred_zeros_coords){
+    for (tuple<int, int> el : starred_zeros_coords) {
+        if ((get<0>(coord) == get<0>(el)) && (get<1>(coord) == get<1>(el))){
+            return true;
+        }
+    }
+    return false;
+}
+
+void print_matrix(int (&Cost)[4][4], int N, int M, vector<tuple<int, int> > starred_zeros_coords){
+    tuple<int, int> coord; 
   for( int i = 0; i < N; ++i){
     for( int j = 0; j < M; ++j) {
-      std::cout<<Cost[i][j]<<'\t';
+        coord = tuple<int, int>(i,j); 
+        if (inlist(coord, starred_zeros_coords)){
+            cout<<Cost[i][j]<<"*"<<'\t';
+        }
+        cout<<Cost[i][j]<<'\t';
     }
-    std::cout<<'\n';
+    cout<<'\n';
   }
 }
 void print_assignment(int N, int* assignment_index){
-  std::cout<<"********Assignment********\n";
+  cout<<"********Assignment********\n";
   for( int i = 0; i < N; ++i){
-      std::cout<<assignment_index[i]<<'\t';
+      cout<<assignment_index[i]<<'\t';
   }
-  std::cout<<'\n';
+  cout<<'\n';
 }
 void assign_tasks(int (&Cost)[4][4], int N, int M, int* assignment_index){
   //check if assignment is possibles
-  std::vector<int> crossed_cols;
+  vector<int> crossed_cols;
   for(int i=0; i<N; i++){
     for (int j = 0; j < M; j++){
-      if (!(std::count(crossed_cols.begin(), crossed_cols.end(), j))){
+      if (!(count(crossed_cols.begin(), crossed_cols.end(), j))){
         if (Cost[i][j] == 0){
             assignment_index[i] = j;
             crossed_cols.push_back(j);
@@ -39,9 +54,9 @@ void assign_tasks(int (&Cost)[4][4], int N, int M, int* assignment_index){
     }
   }
 }
-bool print_check_valid_assignment(int (&Cost)[4][4], int N, int M, int* assignment_index){
+bool print_and_check_valid_assignment(int (&Cost)[4][4], int N, int M, int* assignment_index, vector<tuple<int, int> > starred_zeros_coords){
   assign_tasks(Cost, N, M, assignment_index);
-  print_matrix(Cost, N, M);
+  print_matrix(Cost, N, M, starred_zeros_coords);
   print_assignment(N, assignment_index);
   return check_valid_assignment(assignment_index, N);
 }
