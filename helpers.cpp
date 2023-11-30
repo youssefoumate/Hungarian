052a22ac722580fb3c01ab3b5fcd5bd875fb4281
Youssef OUMATE <ysf.ing.ysf@gmail.com>
2023-11-30 22:44:14 +0900

    finish all steps, more debugging needed

--- helpers.cpp
+++ helpers.cpp
@@ -84,24 +84,3 @@ void print_assignment(int N, int* assignment_index){
   }
   cout<<'\n';
 }
-void assign_tasks(int (&Cost)[4][4], int N, int M, int* assignment_index){
-  //check if assignment is possibles
-  vector<int> crossed_cols;
-  for(int i=0; i<N; i++){
-    for (int j = 0; j < M; j++){
-      if (!(count(crossed_cols.begin(), crossed_cols.end(), j))){
-        if (Cost[i][j] == 0){
-            assignment_index[i] = j;
-            crossed_cols.push_back(j);
-            break;
-        }
-      }
-    }
-  }
-}
-bool print_and_check_valid_assignment(int (&Cost)[4][4], int N, int M, int* assignment_index, vector<tuple<int, int> > starred_zeros_coords, vector<int> marked_columns, vector<tuple<int, int> > primed_zeros_coords, vector<int> marked_rows, vector<tuple<int, int> > path){
-  assign_tasks(Cost, N, M, assignment_index);
-  print_matrix(Cost, N, M, starred_zeros_coords, marked_columns, primed_zeros_coords, marked_rows, path);
-  print_assignment(N, assignment_index);
-  return check_valid_assignment(assignment_index, N);
-}
